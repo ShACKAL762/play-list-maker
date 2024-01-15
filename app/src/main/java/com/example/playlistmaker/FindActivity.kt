d18868d73bebe44d9b17968c2f90b96ca6053f40
Sergey <Cer.97@yandex.ru>
2024-01-15 22:12:37 +0300

    Третья домашняя работа + забытые файлы

--- app/src/main/java/com/example/playlistmaker/FindActivity.kt
+++ app/src/main/java/com/example/playlistmaker/FindActivity.kt
@@ -3,7 +3,13 @@ package com.example.playlistmaker
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.EdgeEffect
+import android.widget.EditText
 import android.widget.ImageButton
+import android.widget.ImageView
 
 
 class FindActivity : AppCompatActivity() {
@@ -12,8 +18,34 @@ class FindActivity : AppCompatActivity() {
         setContentView(R.layout.activity_find)
 
         val backButton = findViewById<ImageButton>(R.id.arrow_back)
+        val searchLine = findViewById<EditText>(R.id.search_line)
+        val clearButton = findViewById<ImageView>(R.id.search_cleaner)
+
+        val simpleTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // empty
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                clearButton.visibility = clearButtonVisibility(s)
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                // empty
+            }
+        }
+        searchLine.addTextChangedListener(simpleTextWatcher)
+
+        clearButton.setOnClickListener { searchLine.setText("") }
         backButton.setOnClickListener{
             finish()
+        }
+    }
+    private fun clearButtonVisibility(s: CharSequence?): Int {
+        return if (s.isNullOrEmpty()) {
+            View.GONE
+        } else {
+            View.VISIBLE
         }
     }
 }
