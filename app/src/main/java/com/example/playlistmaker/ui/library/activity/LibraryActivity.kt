dcad944d28ff370d65f0d56909889a6d743ab18a
Shackal <Cer.97@yandex.ru>
2024-05-11 17:30:32 +0300

    Часть Library

--- app/src/main/java/com/example/playlistmaker/ui/library/activity/LibraryActivity.kt
+++ app/src/main/java/com/example/playlistmaker/ui/library/activity/LibraryActivity.kt
@@ -4,15 +4,19 @@ import android.os.Bundle
 import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 import com.example.playlistmaker.R
+import com.example.playlistmaker.databinding.ActivityLibraryBinding
+import com.example.playlistmaker.databinding.ActivityMainBinding
 
 
 class LibraryActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLibraryBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_library)
 
-        val backButton = findViewById<ImageButton>(R.id.arrow_back)
-        backButton.setOnClickListener{
+        binding = ActivityLibraryBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.arrowBack.setOnClickListener{
             finish()
         }
     }
